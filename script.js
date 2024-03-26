1b2a3273108541dd02734a00793f98546fa5ae92
VikranthCheedella <130175511+VikranthCheedella@users.noreply.github.com>
2024-03-26 11:21:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/";
-
+const BASE_URL="https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
 const dropdown = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
 const fromCurr = document.querySelector(".from select");
@@ -56,11 +55,12 @@ btn.addEventListener("click" , async (evt) => {
         amount.value = "1";
     }
 
-    const URL =`${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+    const URL =`${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
     let response = await fetch(URL);
     let data = await response.json();
-    let rate = data[toCurr.value.toLowerCase()];
-
+    let fromVal = fromCurr.value.toLowerCase();
+    let toVal = toCurr.value.toLowerCase();
+    let rate = data[fromVal][toVal];
     let finalAmount = amtVal * rate;
     msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
     msg.classList.remove("hide");
